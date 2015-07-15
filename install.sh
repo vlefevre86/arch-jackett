58853b1201a16d29dd66aab0a11e25756dd92f0c
Sylvain Desbureaux <sylvain@desbureaux.fr>
2015-07-15 20:18:54 +0200

    moving from building from sources to AUR

--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@
 set -e
 
 # define pacman packages
-pacman_packages="mono nuget git"
+pacman_packages="mono"
 
 # install pre-reqs
 pacman -Sy --noconfirm
 pacman -S --needed $pacman_packages --noconfirm
 
 # Build from source
-source /root/build.sh
+source /root/packer.sh
 
 # cleanup
 yes|pacman -Scc
